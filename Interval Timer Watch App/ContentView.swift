5e0bc2e58f5d4ebbb80ebf208409e9e3fb145c9e
Timothy Breitkreutz <tim@sbrew.com>
2026-05-31 16:12:38 -0400

    Initial Commit

---
+++ Interval Timer Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Interval Timer Watch App
+//
+//  Created by Timothy Breitkreutz on 5/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Interval Timer Watch App/Interval_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_TimerApp.swift
+//  Interval Timer Watch App
+//
+//  Created by Timothy Breitkreutz on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Interval_Timer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
